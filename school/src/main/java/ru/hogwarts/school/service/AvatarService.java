ecc7c7a8dbcbf43b19dcefd00738878181b41212
EKATERINA NATASHENKOVA <vasilieva-ekaterina@yandex.ru>
2025-09-25 21:11:39 +0300

    homework done

--- school/src/main/java/ru/hogwarts/school/service/AvatarService.java
+++ school/src/main/java/ru/hogwarts/school/service/AvatarService.java
@@ -1,5 +1,7 @@
 package ru.hogwarts.school.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -27,6 +29,8 @@ import static java.nio.file.StandardOpenOption.CREATE_NEW;
 @Service
 public class AvatarService {
 
+    private static final Logger logger = LoggerFactory.getLogger(AvatarService.class);
+
     @Value("${avatars.dir.path}")
     private String avatarsDir;
 
@@ -39,11 +43,14 @@ public class AvatarService {
     }
 
     public void uploadAvatar(Long studentId, MultipartFile file) throws IOException {
+        logger.info("Was invoked method for upload avatar");
+
         Student student = studentRepository.getById(studentId);
 
         Path filePath = Path.of(avatarsDir, studentId + "." + getExtension(file.getOriginalFilename()));
         Files.createDirectories(filePath.getParent());
         Files.deleteIfExists(filePath);
+            logger.debug("The file will be saved at path: {}, extension: {}", filePath, getExtension(file.getOriginalFilename()));
 
         try (InputStream is = file.getInputStream();
              OutputStream os = Files.newOutputStream(filePath, CREATE_NEW);
@@ -51,6 +58,7 @@ public class AvatarService {
              BufferedOutputStream bos = new BufferedOutputStream(os, 1024);
         ) {
             bis.transferTo(bos);
+            logger.debug("File saved successfully. Path: {}, size: {} bytes", filePath, file.getSize());
         }
         /**
          * альтернативный вариант для строк 42-48 (вместо блока try-with-resources)
@@ -59,6 +67,7 @@ public class AvatarService {
 
         Avatar avatar = findAvatar(studentId);
         if (avatar == null) {
+            logger.debug("Creating a new avatar entry for a student with ID: {}", studentId);
             avatar = new Avatar();
         }
         avatar.setStudent(student);
@@ -68,22 +77,31 @@ public class AvatarService {
         avatar.setData(file.getBytes());
 
         avatarRepository.save(avatar);
+        logger.debug("Updating an existing avatar entry for a student with ID: {}", studentId);
     }
 
     public Avatar findAvatar(long studentId) {
+        logger.info("Was invoked method for search avatar by student id");
+        logger.warn("Avatar by student with ID {} not found", studentId);
         return avatarRepository.findByStudentId(studentId).orElseGet(Avatar::new);
     }
 
     private String getExtension(String fileName) {
+        logger.info("Was invoked method for get extension");
+        logger.debug("Get extension file: {}", fileName);
         return fileName.substring(fileName.lastIndexOf(".") + 1);
     }
 
     public Collection<Avatar> getAll() {
+        logger.info("Was invoked method for get all avatars");
+        logger.debug("Request all avatars");
         return avatarRepository.findAll();
     }
 
     public Collection<Avatar> getAllAvatarsByPage(Integer pageNumber, Integer pageSize) {
+        logger.info("Was invoked method for get all avatars by page");
         PageRequest pageRequest = PageRequest.of(pageNumber,pageSize);
+        logger.debug("Request for avatars. Page: {}, page size: {}", pageNumber, pageSize);
         return avatarRepository.findAll(pageRequest).getContent();
     }
 
--- school/src/main/java/ru/hogwarts/school/service/FacultyService.java
+++ school/src/main/java/ru/hogwarts/school/service/FacultyService.java
@@ -1,5 +1,7 @@
 package ru.hogwarts.school.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.hogwarts.school.model.Faculty;
@@ -13,6 +15,8 @@ import java.util.stream.Collectors;
 @Service
 public class FacultyService {
 
+    private static final Logger logger = LoggerFactory.getLogger(FacultyService.class);
+
     private final FacultyRepository facultyRepository;
 
     public FacultyService(FacultyRepository facultyRepository) {
@@ -20,32 +24,47 @@ public class FacultyService {
     }
 
     public Faculty createFaculty(Faculty faculty) {
+        logger.info("Was invoked method for faculty student");
+        logger.debug("Was create faculty with ID: {}", faculty.getId());
         return facultyRepository.save(faculty);
     }
 
     public Faculty getFacultyId(Long id) {
+        logger.info("Was invoked method for get faculty by id");
+        logger.warn("Faculty with ID {} not faund", id);
         return facultyRepository.findById(id).get();
     }
 
     public Faculty updateFaculty(Faculty faculty) {
+        logger.info("Was invoked method for update faculty");
+        logger.warn("Attempt to update a non-existent faculty with ID: {}", faculty.getId());
         return facultyRepository.save(faculty);
     }
 
     public void deleteFaculty(Long id) {
+        logger.info("Was invoked method for delete faculty by id");
+        logger.debug("Faculty with ID {} successfully removed", id);
         facultyRepository.deleteById(id);
     }
 
     public Collection<Faculty> filterColor(String color) {
+        logger.info("Was invoked method for filtering faculty by color");
+        logger.debug("Filtering faculty by color: {}", color);
         return facultyRepository.findAll().stream()
                 .filter(faculty -> Objects.equals(faculty.getColor(), color))
                 .collect(Collectors.toList());
     }
 
     public Collection<Faculty> findByNameOrColor(String findParameter) {
+        logger.info("Was invoked method for search faculty by name or color");
+        logger.debug("Search faculty by name or color - findParameter: {}", findParameter);
         return facultyRepository.findByNameOrColorIgnoreCase(findParameter, findParameter);
     }
 
     public Collection<Student> getStudentsByFaculty(Long id) {
+        logger.info("Was invoked method for get students faculty by id");
+        logger.debug("Get students faculty by ID: {}", id);
         return facultyRepository.findById(id).map(Faculty::getStudents).orElseThrow();
     }
+
 }
